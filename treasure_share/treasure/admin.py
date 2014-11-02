191ec99b198ef629df4e9f76b1556a372f3f74ac
Kevin Lin <kevinlin@berkeley.edu>
2014-11-02 10:33:20 -0500

    i really messed that move up

---
+++ treasure_share/treasure/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from treasure.models import Donation, Dribble, Profile, Recipients, Membership
+
+admin.site.register(Donation)
+admin.site.register(Dribble)
+admin.site.register(Profile)
+admin.site.register(Recipients)
+admin.site.register(Membership)
---
+++ treasure_share/treasure_share/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from treasure import views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'treasure_share.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^donate/$', views.donate, name='donate'),
+    url(r'^donate/action/', views.action, name='action'),
+    url(r'^auth/', views.auth),
+    url(r'^auth2/', views.auth2),
+    url(r'^display_oauth/', views.display_oauth),
+)
